234bb306d4b5696da6047b1d1441989a6511fe02
oxinet <gordon@oxinet.co.uk>
2014-02-27 17:04:02 +0000

    Initial commit of wg-wp.php
    Main plugin php file

---
+++ eg-wp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package EG_WP
+ * @version 0.8
+ */
+/*
+Plugin Name: EG WordPress API
+Plugin URI: http://wordpress.org/extend/plugins/eg-wp/
+Description: 
+Author: Oliver Chalk - Oxinet
+Version: 0.8
+Author URI: http://oxi.net
+*/
+
+?>
