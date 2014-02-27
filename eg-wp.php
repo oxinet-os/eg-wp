679e446ebf9ec5b1b23e8a5579cb214254a7ef75
oxinet <gordon@oxinet.co.uk>
2014-02-27 17:14:29 +0000

    Adding licencing to PHP file

--- eg-wp.php
+++ eg-wp.php
@@ -4,12 +4,28 @@
  * @version 0.8
  */
 /*
-Plugin Name: EG WordPress API
-Plugin URI: http://wordpress.org/extend/plugins/eg-wp/
-Description: 
-Author: Oliver Chalk - Oxinet
-Version: 0.8
-Author URI: http://oxi.net
+	Plugin Name: EG WordPress API
+	Plugin URI: http://wordpress.org/extend/plugins/eg-wp/
+	Description: 
+	Author: Oliver Chalk - Oxinet
+	Version: 0.8
+	Author URI: http://oxi.net
+	Licence: GNU General Public License, version 3 (GPL-3.0)
+
+	Copyright 2014 Oliver Chalk - Oxinet (email : ollie@oxi.net)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 ?>
